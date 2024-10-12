46c0a613b065783e3020da8695737999bec887e4
James Santucci <james.santucci@gmail.com>
2024-10-12 11:08:58 -0700

    Add Ch1 checklist implementation
    Am I missing something? For whatever reason, the naive implementation,
    which is supposed to be n^2, looks like it's just n, while the checklist
    implementation is getting wrecked by the naive implementation (it's
    consistently about half as fast per execution).
    
    My best guess is GHC is optimizing something in the naive
    implementation (but what??), while the two list traversals (the filter
    and the takeWhile) are costing the checklist implementation a lot. It
    could also be my sparse input (max 10% range coverage) vs. a dense input
    maybe.

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Ch1MinFree (minFree)
+import Ch1MinFree (minFree', naiveMinFree)
 import Control.Monad (replicateM)
 import Criterion.Main (bench, bgroup, defaultMain, env, whnf)
 import System.Random (randomRIO)
@@ -8,28 +8,38 @@ import System.Random (randomRIO)
 randomIntList :: Int -> Int -> IO [Int]
 randomIntList rangeMax n = replicateM n (randomRIO (0, rangeMax))
 
-getEnv :: IO ([Int], [Int], [Int], [Int])
+getEnv :: IO ([Int], [Int], [Int], [Int], [Int])
 getEnv =
-  (,,,)
+  (,,,,)
     <$> randomIntList 1000 100
     <*> randomIntList 10000 1000
     <*> randomIntList 100000 10000
     <*> randomIntList 1000000 100000
+    <*> randomIntList 10000000 1000000
 
 main :: IO ()
 main =
   defaultMain
-    [ bgroup
-        "minFree"
-        [ env
-            getEnv
-            $ \ ~(smallList, bigList, biggerList, hugeList) ->
+    [ env
+        getEnv
+        $ \ ~(smallList, bigList, biggerList, hugeList, hugerList) ->
+          bgroup
+            "minFree"
+            [ bgroup
+                "checklist"
+                [ bench "small" $ whnf minFree' smallList,
+                  bench "big" $ whnf minFree' bigList,
+                  bench "bigger" $ whnf minFree' biggerList,
+                  bench "huge" $ whnf minFree' hugeList,
+                  bench "huger" $ whnf minFree' hugerList
+                ],
               bgroup
                 "naive"
-                [ bench "small" $ whnf minFree smallList,
-                  bench "big" $ whnf minFree bigList,
-                  bench "bigger" $ whnf minFree biggerList,
-                  bench "huge" $ whnf minFree hugeList
+                [ bench "small" $ whnf naiveMinFree smallList,
+                  bench "big" $ whnf naiveMinFree bigList,
+                  bench "bigger" $ whnf naiveMinFree biggerList,
+                  bench "huge" $ whnf naiveMinFree hugeList,
+                  bench "huger" $ whnf naiveMinFree hugerList
                 ]
-        ]
+            ]
     ]
--- src/Ch1MinFree.hs
+++ src/Ch1MinFree.hs
@@ -1,9 +1,40 @@
+{-# LANGUAGE TupleSections #-}
+
 module Ch1MinFree where
+
+import Data.Array (Array, accumArray, elems)
+
+-- Round 1: Naive
+-- diff the list of actual numbers with the list of
+-- numbers from 0 to infinity (thanks laziness!)
 
 -- | Find elements in one list not present in another list.
 (\\) :: (Eq a) => [a] -> [a] -> [a]
 us \\ vs = filter (`notElem` vs) us
 
 -- | Find the first natural number not present in xs
-minFree :: [Int] -> Int
-minFree xs = head ([0 ..] \\ xs)
+naiveMinFree :: [Int] -> Int
+naiveMinFree xs = head ([0 ..] \\ xs)
+
+-- Round 2: checklist
+-- for a given list xs, the minimum free number must be less than
+-- length xs, so we can check each number 0 .. length xs then stop
+-- when we find one that's missing
+
+-- | Find the index of the first non-True entry
+search :: Array Int Bool -> Int
+search = length . takeWhile id . elems
+-- ^ Array i a is an array of values with index type i and values a, I think
+
+checklist :: [Int] -> Array Int Bool
+checklist xs =
+  accumArray
+    (||) -- rule for combining values
+    False -- initial value at each index
+    (0, listLength) -- range of indices
+    ((,True) <$> filter (<= listLength) xs) -- (index, value) pairs
+  where
+    listLength = length xs
+
+minFree' :: [Int] -> Int
+minFree' xs = search (checklist xs)

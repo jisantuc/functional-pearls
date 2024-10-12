7ab88beb139bd751de3740da4277b619bd30b282
James Santucci <james.santucci@gmail.com>
2024-10-12 11:26:47 -0700

    Benchmark sparse and dense envs for minFree implementations

--- bench/Main.hs
+++ bench/Main.hs
@@ -2,14 +2,21 @@ module Main where
 
 import Ch1MinFree (minFree', naiveMinFree)
 import Control.Monad (replicateM)
-import Criterion.Main (bench, bgroup, defaultMain, env, whnf)
+import Criterion.Main
+  ( Benchmark,
+    bench,
+    bgroup,
+    defaultMain,
+    env,
+    whnf,
+  )
 import System.Random (randomRIO)
 
 randomIntList :: Int -> Int -> IO [Int]
 randomIntList rangeMax n = replicateM n (randomRIO (0, rangeMax))
 
-getEnv :: IO ([Int], [Int], [Int], [Int], [Int])
-getEnv =
+getSparseEnv :: IO ([Int], [Int], [Int], [Int], [Int])
+getSparseEnv =
   (,,,,)
     <$> randomIntList 1000 100
     <*> randomIntList 10000 1000
@@ -17,29 +24,44 @@ getEnv =
     <*> randomIntList 1000000 100000
     <*> randomIntList 10000000 1000000
 
+getDenseEnv :: IO ([Int], [Int], [Int], [Int], [Int])
+getDenseEnv =
+  (,,,,)
+    <$> randomIntList 100 100
+    <*> randomIntList 1000 1000
+    <*> randomIntList 10000 10000
+    <*> randomIntList 100000 100000
+    <*> randomIntList 1000000 1000000
+
 main :: IO ()
 main =
   defaultMain
-    [ env
-        getEnv
-        $ \ ~(smallList, bigList, biggerList, hugeList, hugerList) ->
-          bgroup
-            "minFree"
-            [ bgroup
-                "checklist"
-                [ bench "small" $ whnf minFree' smallList,
-                  bench "big" $ whnf minFree' bigList,
-                  bench "bigger" $ whnf minFree' biggerList,
-                  bench "huge" $ whnf minFree' hugeList,
-                  bench "huger" $ whnf minFree' hugerList
-                ],
-              bgroup
-                "naive"
-                [ bench "small" $ whnf naiveMinFree smallList,
-                  bench "big" $ whnf naiveMinFree bigList,
-                  bench "bigger" $ whnf naiveMinFree biggerList,
-                  bench "huge" $ whnf naiveMinFree hugeList,
-                  bench "huger" $ whnf naiveMinFree hugerList
-                ]
-            ]
+    [ bgroup
+        "minFree"
+        [ minFreeBench "sparse" getSparseEnv,
+          minFreeBench "dense" getDenseEnv
+        ]
     ]
+
+minFreeBench :: String -> IO ([Int], [Int], [Int], [Int], [Int]) -> Benchmark
+minFreeBench label envIO =
+  env envIO $ \ ~(smallList, bigList, biggerList, hugeList, hugerList) ->
+    bgroup
+      label
+      [ bgroup
+          "checklist"
+          [ bench "small" $ whnf minFree' smallList,
+            bench "big" $ whnf minFree' bigList,
+            bench "bigger" $ whnf minFree' biggerList,
+            bench "huge" $ whnf minFree' hugeList,
+            bench "huger" $ whnf minFree' hugerList
+          ],
+        bgroup
+          "naive"
+          [ bench "small" $ whnf naiveMinFree smallList,
+            bench "big" $ whnf naiveMinFree bigList,
+            bench "bigger" $ whnf naiveMinFree biggerList,
+            bench "huge" $ whnf naiveMinFree hugeList,
+            bench "huger" $ whnf naiveMinFree hugerList
+          ]
+      ]
